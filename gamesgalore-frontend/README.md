791ac66866c208bdc4933d20bb8294368f495762
Vicente <you@example.com>
2019-03-25 16:47:10 -0400

    landing page started

--- gamesgalore-frontend/README.md
+++ gamesgalore-frontend/README.md
@@ -66,3 +66,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+* Start landing page
+* Make the rows with system names
--- gamesgalore-frontend/src/App.js
+++ gamesgalore-frontend/src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let currentSystems = ["Playstation 4", "Xbox One", "Nintendo Switch", "Mobile"]
+    let systemRow = currentSystems.map(system =>
+      <div className="col">
+        <button className="btn">{system}</button>
+      </div>
+    )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
+          <button className="btn btn-default">gamesGalore</button>
+        </div>
+        <div className="row">
+            {systemRow}
+        </div>
       </div>
     );
   }
