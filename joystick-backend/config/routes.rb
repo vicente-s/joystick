35f4d58c02152b230805715e18895ff48dd05efd
Vicente <you@example.com>
2019-04-03 19:40:32 -0400

    get request for games

--- joystick-backend/config/routes.rb
+++ joystick-backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # resources :games
   # resources :releases
-  resources :systems
+  resources :systems, only: [:index]
+  resources :games, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
