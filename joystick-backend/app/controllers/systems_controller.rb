f4452641609bc1095d11f767dcd3cc12d98e8e64
Vicente <you@example.com>
2019-04-07 21:47:54 -0400

    key added

--- joystick-backend/app/controllers/systems_controller.rb
+++ joystick-backend/app/controllers/systems_controller.rb
@@ -3,7 +3,7 @@ class SystemsController < ApplicationController
   def index
     require 'net/https'
     http = Net::HTTP.new('api-v3.igdb.com', 80)
-    request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => ''})
+    request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => "#{ENV['IGDB_API']}"})
     request.body = 'fields abbreviation,alternative_name,category,created_at,generation,name,platform_logo,product_family,slug,summary,updated_at,url,versions,websites;'
     render json: http.request(request).body
   end
--- joystick-backend/config/routes.rb
+++ joystick-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :games
   # resources :releases
-  resources :systems, only: [:index]
-  resources :games, only: [:index]
+  resources :systems, only: [:index, :show]
+  resources :games, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
