d5ce13c193480b67a413f9bc5c75be3171c2f376
Vicente <you@example.com>
2019-04-23 18:54:51 -0400

    backend runs a little faster

--- joystick-backend/app/controllers/systems_controller.rb
+++ joystick-backend/app/controllers/systems_controller.rb
@@ -1,8 +1,12 @@
 class SystemsController < ApplicationController
+  require 'net/https'
 
   def index
-    @systems = System.all
-    render json: @systems
+    require 'net/https'
+    http = Net::HTTP.new('api-v3.igdb.com', 80)
+    request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => "#{ENV['IGDB_API']}"})
+    request.body = 'fields abbreviation,alternative_name,category,created_at,generation,name,platform_logo,product_family,slug,summary,updated_at,url,versions,websites;'
+    render json: http.request(request).body
   end
 
   def show
--- joystick-backend/db/seeds.rb
+++ joystick-backend/db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-require 'net/https'
-http = Net::HTTP.new('api-v3.igdb.com', 80)
-request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => "#{ENV[IGDB_API]}"})
-request.body = 'fields abbreviation,alternative_name,category,created_at,generation,name,platform_logo,product_family,slug,summary,updated_at,url,versions,websites;'
-
-platforms_response = http.request(request).body
-parsed_platforms = JSON.parse(platforms_response)
-parsed_platforms.each { |platform|
-  System.create({name: platform["name"], summary: platform["summary"], platform_logo_id:platform["platform_logo"]})
-  # platform_logo
-  # summary
-}
